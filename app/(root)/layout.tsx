f453ca01d1daadb702874c6ec7380a227ef3376e
Richky Abednego <richky.abednego@gmail.com>
2025-01-29 14:04:07 +0700

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import "../globals.css";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main>
+            SIDEBAR
+            {children}
+        </main>
+    );
+}
+
